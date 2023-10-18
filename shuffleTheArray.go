d76ce9e0cc9783d5216eb1c7139ae60ce4abac3b
ssengerb <sanzhar_1710@mail.ru>
2023-10-18 23:40:36 +0600

    go

---
+++ shuffleTheArray.go
@@ -0,0 +1,10 @@
+package leetcode
+
+func shuffle(nums []int, n int) []int {
+	result := make([]int, 2*n)
+	for i := 0; i < n; i++ {
+		result[i+i] = nums[i]
+		result[2*i+1] = nums[i+n]
+	}
+	return result
+}
