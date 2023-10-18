fee45a34a8c7803c5f0ac6b68f795f55c7643e3b
ssengerb <sanzhar_1710@mail.ru>
2023-10-18 23:44:39 +0600

    go

---
+++ numberOfArithmeticTriplets.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func arithmeticTriplets(nums []int, diff int) int {
+	count := 0
+	for i := 0; i < len(nums)-2; i++ {
+		for j := i + 1; j < len(nums)-1; j++ {
+			for k := j + 1; k < len(nums); k++ {
+				if nums[j]-nums[i] == diff && nums[k]-nums[j] == diff {
+					count++
+				}
+			}
+		}
+	}
+	return count
+}
