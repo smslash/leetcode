ae4cec6f49d56d55608fa6b57df947a943ec3e0f
ssengerb <sanzhar_1710@mail.ru>
2023-10-18 23:29:54 +0600

    go

---
+++ removeElement.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func removeElement(nums []int, val int) int {
+	j := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[j], nums[i] = nums[i], nums[j]
+			j++
+		}
+	}
+	return j
+}
