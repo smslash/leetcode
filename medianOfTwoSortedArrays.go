e62704c35b12d72aff5073cc69dca384c5c5970d
ssengerb <sanzhar_1710@mail.ru>
2023-10-18 23:27:04 +0600

    go

---
+++ medianOfTwoSortedArrays.go
@@ -0,0 +1,60 @@
+package leetcode
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	if len(nums1) > len(nums2) {
+		nums1, nums2 = nums2, nums1
+	}
+
+	m, n := len(nums1), len(nums2)
+	imin, imax, halfLen := 0, m, (m+n+1)/2
+	maxLeft, minRight := 0, 0
+
+	for imin <= imax {
+		i := (imin + imax) / 2
+		j := halfLen - i
+
+		if i < m && nums2[j-1] > nums1[i] {
+			imin = i + 1
+		} else if i > 0 && nums1[i-1] > nums2[j] {
+			imax = i - 1
+		} else {
+			if i == 0 {
+				maxLeft = nums2[j-1]
+			} else if j == 0 {
+				maxLeft = nums1[i-1]
+			} else {
+				maxLeft = max(nums1[i-1], nums2[j-1])
+			}
+
+			if (m+n)%2 == 1 {
+				return float64(maxLeft)
+			}
+
+			if i == m {
+				minRight = nums2[j]
+			} else if j == n {
+				minRight = nums1[i]
+			} else {
+				minRight = min(nums1[i], nums2[j])
+			}
+
+			return float64(maxLeft+minRight) / 2.0
+		}
+	}
+
+	return 0.0
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
