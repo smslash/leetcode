74b7da484579dd3b6006c4a945d4d4859ae12029
Sanzhar Sengerbayev <78522133+smslash@users.noreply.github.com>
2023-10-24 00:54:45 +0600

    go

---
+++ ZigzagConversion.go
@@ -0,0 +1,30 @@
+func convert(s string, numRows int) string {
+    zigzag := make([][]rune, numRows)
+    
+    i := 0
+    col := 0
+    for i < len(s) {
+        for row := 0; row < numRows; row++ {
+            zigzag[row] = append(zigzag[row], rune(s[i]))
+            i++
+            if i >= len(s) {
+                break
+            }
+        }
+        
+        for row := numRows - 2; row > 0; row-- {
+            if i >= len(s) {
+                break
+            }
+            zigzag[row] = append(zigzag[row], rune(s[i]))
+            col++
+            i++
+        }
+    }
+	
+    zigzagStr := ""             
+    for row := 0; row < numRows; row++ {
+        zigzagStr += string(zigzag[row])    
+    }
+    return zigzagStr
+}
