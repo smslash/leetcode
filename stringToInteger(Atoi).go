de9da6b64f3fe10cd14552cc9b26b60d866d0fdd
Sanzhar Sengerbayev <78522133+smslash@users.noreply.github.com>
2023-10-26 23:13:16 +0600

    go

---
+++ stringToInteger(Atoi).go
@@ -0,0 +1,34 @@
+func myAtoi(s string) int {
+    start := false
+    flag := 1
+    res := 0
+    for _,v := range s {
+        if v == ' ' && !start {
+            continue
+        }
+        if v == '-' && !start {
+            flag = -1
+            start = true
+            continue
+        }
+        if v == '+' && !start {
+            start = true
+            continue
+        }
+        
+        if v >= 48 && v <= 57 {
+            start = true
+            digit := int(v - '0') * flag
+            if res > math.MaxInt32/10 || (res == math.MaxInt32/10 && digit > 7) {
+                return math.MaxInt32
+            }
+            if res < math.MinInt32/10 || (res == math.MinInt32/10 && digit < -8) {
+                return math.MinInt32
+            }
+            res = res*10 + digit
+        } else {
+            break
+        }
+    }
+    return res
+}
