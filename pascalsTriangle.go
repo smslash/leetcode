11aff69644712d6d0b23c8b0aa1b6cbcccc46578
Sanzhar Sengerbayev <78522133+smslash@users.noreply.github.com>
2023-11-05 16:20:13 +0600

    go

---
+++ pascalsTriangle.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func generate(numRows int) [][]int {
+    res := make([][]int, numRows)
+    
+    for i := 0; i < numRows; i++ {
+        res[i] = make([]int, i+1)
+    }
+
+    res[0][0] = 1
+    
+    for r := 1; r < numRows; r++ {
+        for c := 0; c < len(res[r]); c++ {
+            if c == 0 || c == len(res[r]) - 1 {
+                res[r][c] = 1
+            } else {
+                res[r][c] = res[r-1][c-1] + res[r-1][c]  
+            }
+        }
+    }
+    
+    return res
+}
