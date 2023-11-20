53342b84aa796b2a6991356311631df79140e086
Sanzhar Sengerbayev <78522133+smslash@users.noreply.github.com>
2023-11-20 16:58:05 +0600

    25

--- minimumAmountOfTimeToCollectGarbage.go
+++ minimumAmountOfTimeToCollectGarbage.go
@@ -1,7 +1,4 @@
-package leetcode
-
 func garbageCollection(garbage []string, travel []int) int {
-	mTruck, pTruck, gTruck := 0, 0, 0
 	mCount, pCount, gCount := 0, 0, 0
 	mIndex, pIndex, gIndex := 0, 0, 0
 	for i := 0; i < len(garbage); i++ {
@@ -19,37 +16,19 @@ func garbageCollection(garbage []string, travel []int) int {
 			}
 		}
 	}
-
-	for i := 0; i < len(garbage); i++ {
-		for j := 0; j < len(garbage[i]); j++ {
-			switch garbage[i][j] {
-			case 'M':
-				mTruck++
-			case 'P':
-				pTruck++
-			case 'G':
-				gTruck++
-			}
-		}
-	}
-
-	// fmt.Println(mCount + pCount + gCount)
-
+    
 	res := 0
 	for i := 0; i < len(travel); i++ {
 		if i <= mIndex-1 && mIndex != 0 {
 			res += travel[i]
-			// fmt.Printf("M: %d\n", i)
 		}
 
 		if i <= pIndex-1 && pIndex != 0 {
 			res += travel[i]
-			// fmt.Printf("P: %d\n", i)
 		}
 
 		if i <= gIndex-1 && gIndex != 0 {
 			res += travel[i]
-			// fmt.Printf("G: %d\n", i)
 		}
 	}
 
