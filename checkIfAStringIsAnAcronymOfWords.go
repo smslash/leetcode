1079c38d67c31642a853b63fdd17f97bfaddb941
Sanzhar Sengerbayev <78522133+smslash@users.noreply.github.com>
2023-11-30 19:43:45 +0600

    4

---
+++ checkIfAStringIsAnAcronymOfWords.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func isAcronym(words []string, s string) bool {
+	if len(words) != len(s) {
+		return false
+	}
+    for i := 0; i < len(words); i++ {
+		if words[i][0] != s[i] {
+			return false
+		}
+	}
+	return true
+}
