4222db5e900a9d32aa55b049dac53b1dd081f933
Sanzhar Sengerbayev <78522133+smslash@users.noreply.github.com>
2023-12-13 07:05:35 +0600

    2

---
+++ countingWordsWithAGivenPrefix.go
@@ -0,0 +1,11 @@
+package leetcode
+
+func prefixCount(words []string, pref string) int {
+    res := 0
+    for i := 0; i < len(words); i++ {
+        if len(words[i]) >= len(pref) && words[i][:len(pref)] == pref {
+            res++
+        }
+    }
+    return res
+}
