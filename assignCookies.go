ca444e5195781115f69dd55f55649a0604a81f7d
Sanzhar Sengerbayev <78522133+smslash@users.noreply.github.com>
2024-01-01 23:10:30 +0600

    5

---
+++ assignCookies.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func findContentChildren(g []int, s []int) int {
+    sort.Ints(g)
+    sort.Ints(s)
+    
+    contentChildren := 0
+    cookieIndex := 0
+
+    for cookieIndex < len(s) && contentChildren < len(g) {
+        if s[cookieIndex] >= g[contentChildren] {
+            contentChildren++
+        }
+        cookieIndex++
+    }
+
+    return contentChildren
+}
