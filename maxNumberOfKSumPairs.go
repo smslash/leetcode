9a7aac2f05a55495ecdacfc412014dfcbbfc8e47
Sanzhar Sengerbayev <78522133+smslash@users.noreply.github.com>
2024-01-03 06:30:18 +0600

    12

---
+++ maxNumberOfKSumPairs.go
@@ -0,0 +1,21 @@
+package leetcode
+
+func maxOperations(nums []int, k int) int {
+	sort.Ints(nums)
+	left, right := 0, len(nums)-1
+	count := 0
+
+	for left < right {
+		sum := nums[left] + nums[right]
+		if sum == k {
+			count++
+			left++
+			right--
+		} else if sum < k {
+			left++
+		} else {
+			right--
+		}
+	}
+	return count
+}
